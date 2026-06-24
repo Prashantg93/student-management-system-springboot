af09b35fee26d9935356ab31df2d37a3c0b70e08
Prashant <gholapprashant6@gmail.com>
2026-06-24 20:45:51 +0530

    add files

--- src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -4,33 +4,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.repository.StudentRepository;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication implements CommandLineRunner{
+public class StudentManagementSystemApplication extends SpringBootServletInitializer implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
+	 @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder 
+	   application) {
+	      return application.sources(StudentManagementSystemApplication.class);
+	   }
+	 
 	@Autowired
 	private StudentRepository studentRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		/*
-		 * Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
-		 * studentRepository.save(student1);
-		 * 
-		 * Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
-		 * studentRepository.save(student2);
-		 * 
-		 * Student student3 = new Student("tony", "stark", "tony@gmail.com");
-		 * studentRepository.save(student3);
-		 */
+		
+		  Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
+		  studentRepository.save(student1);
+		  
+		  Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+		  studentRepository.save(student2);
+		  
+		  Student student3 = new Student("tony", "stark", "tony@gmail.com");
+		  studentRepository.save(student3);
+		 
 		
 	}
 
