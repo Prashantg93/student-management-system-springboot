460c8a2d1440d2c4ae899ba76900b95a053dc723
Prashant <gholapprashant6@gmail.com>
2026-06-24 20:36:32 +0530

    add files

--- src/main/java/net/javaguides/sms/entity/Student.java
+++ src/main/java/net/javaguides/sms/entity/Student.java
@@ -1,6 +1,11 @@
 package net.javaguides.sms.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "students")
